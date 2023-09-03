2e4f450b53c39949b246ac380c8812bd516342a8
Power <zzw890827@gmail.com>
2023-09-04 00:22:28 +0900

    update new result

--- src/rate.js
+++ src/rate.js
@@ -48,7 +48,7 @@ option = {
   },
   xAxis: {
     type: 'category',
-    data: ['第一回', '第二回', '第三回', '第四回', '第五回'],
+    data: ['第一回', '第二回'],
   },
   yAxis: {
     type: 'value',
@@ -59,47 +59,7 @@ option = {
   },
   series: [
     {
-      data: [76.3, 85.5, 89.5, 93.4, 100],
-      type: 'line',
-      markLine: {
-        symbol: ['none', 'none'],
-        label: { show: true },
-        data: [{ yAxis: 75 }],
-      },
-      markPoint: {
-        data: [
-          { type: 'max', name: '最大值' },
-          { type: 'min', name: '最小值' },
-        ],
-      },
-    },
-  ],
-};
-
-//
-
-option = {
-  title: {
-    left: 'center',
-    text: '测试3正确率推移',
-  },
-  tooltip: {
-    trigger: 'axis',
-  },
-  xAxis: {
-    type: 'category',
-    data: ['第一回', '第二回', '第三回', '第四回', '第五回'],
-  },
-  yAxis: {
-    type: 'value',
-    scale: true,
-    axisLabel: {
-      formatter: '{value}%',
-    },
-  },
-  series: [
-    {
-      data: [69.7, 90.8, 94.7, 93.4, 98.7],
+      data: [50, 75.9],
       type: 'line',
       markLine: {
         symbol: ['none', 'none'],
